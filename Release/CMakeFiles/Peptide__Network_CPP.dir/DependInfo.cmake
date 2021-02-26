e649e880a2850f0a0e2165d5535be6b9cff76d72
Omar <omar.taha.mohammed@gmail.com>
2021-02-27 02:00:31 +0300

    make

--- Release/CMakeFiles/Peptide__Network_CPP.dir/DependInfo.cmake
+++ Release/CMakeFiles/Peptide__Network_CPP.dir/DependInfo.cmake
@@ -4,15 +4,15 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/omarsulaivany/Desktop/Gitkraken/Peptide_Network_CPP/include/Net.cpp" "/home/omarsulaivany/Desktop/Gitkraken/Peptide_Network_CPP/Release/CMakeFiles/Peptide__Network_CPP.dir/include/Net.cpp.o"
-  "/home/omarsulaivany/Desktop/Gitkraken/Peptide_Network_CPP/include/Neuron.cpp" "/home/omarsulaivany/Desktop/Gitkraken/Peptide_Network_CPP/Release/CMakeFiles/Peptide__Network_CPP.dir/include/Neuron.cpp.o"
-  "/home/omarsulaivany/Desktop/Gitkraken/Peptide_Network_CPP/src/main.cpp" "/home/omarsulaivany/Desktop/Gitkraken/Peptide_Network_CPP/Release/CMakeFiles/Peptide__Network_CPP.dir/src/main.cpp.o"
+  "/home/omar/Desktop/GitKraken/Peptide_Network_CPP/include/Net.cpp" "/home/omar/Desktop/GitKraken/Peptide_Network_CPP/Release/CMakeFiles/Peptide__Network_CPP.dir/include/Net.cpp.o"
+  "/home/omar/Desktop/GitKraken/Peptide_Network_CPP/include/Neuron.cpp" "/home/omar/Desktop/GitKraken/Peptide_Network_CPP/Release/CMakeFiles/Peptide__Network_CPP.dir/include/Neuron.cpp.o"
+  "/home/omar/Desktop/GitKraken/Peptide_Network_CPP/src/main.cpp" "/home/omar/Desktop/GitKraken/Peptide_Network_CPP/Release/CMakeFiles/Peptide__Network_CPP.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include/opencv4"
+  "/usr/include/opencv4"
   )
 
 # Targets to which this target links.
--- Release/cmake_install.cmake
+++ Release/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/omarsulaivany/Desktop/Gitkraken/Peptide_Network_CPP
+# Install script for directory: /home/omar/Desktop/GitKraken/Peptide_Network_CPP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/omarsulaivany/Desktop/Gitkraken/Peptide_Network_CPP/Release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/omar/Desktop/GitKraken/Peptide_Network_CPP/Release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
